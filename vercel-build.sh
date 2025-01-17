3ec25edbd510c3a94a0b505571fc68f7b3233382
Michiel Brand <michiel@etse.co.za>
2025-01-17 10:32:12 +0200

    updated versel script

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # default versions
-NODE_VERSION='18.16.1';
-GO_VERSION='1.23.3';
-HUGO_VERSION='0.139.2';
+NODE_VERSION='23.6.0';
+GO_VERSION='1.23.4';
+HUGO_VERSION='0.140.2';
 
 # install Node.js
-# echo "Installing Node.js $NODE_VERSION..."
-# curl -sSOL https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
-# tar -xzf node-v${NODE_VERSION}.tar.gz
-# export PATH=$PATH:/usr/local/bin
-# rm -rf node-v${NODE_VERSION}.tar.gz
+echo "Installing Node.js $NODE_VERSION..."
+curl -sSOL https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
+tar -xzf node-v${NODE_VERSION}.tar.gz
+export PATH=$PATH:/usr/local/bin
+rm -rf node-v${NODE_VERSION}.tar.gz
 
 echo "USING NODE VERSION: $(node -v)"
 
